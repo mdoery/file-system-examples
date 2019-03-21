dfbaa8165e4abf604e9b6872994cc09d1187e620
Marya Doery <mdoery@gmail.com>
2019-03-21 12:31:19 -0400

    New copyFile method, with passing tests.

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ var isFilePromise = function(filename, resolve, reject) {
 	});
 }
 
+var copyFilePromise = function(fromFilename, toFilename, resolve, reject) {
+	var result = fs.copyFile(fromFilename, toFilename, fs.constants.COPYFILE_EXCL, function(err, result) {
+		console.log("result = " + result);
+		if (err) {
+			reject(err);
+		} else {
+			resolve();
+		}
+	});
+}
+
 /**
  * @param {String} name of file to test for existence
  * @return {Boolean} true if input file name refers to a file, otherwise false.
@@ -22,6 +33,18 @@ var isFile = (filename) => new Promise(function(resolve, reject) {
 	}, 1000);
 })
 
+/**
+ * Copy the file named fromFilename to the file named toFilename. Will fail
+ * if toFilename already exists.
+ * @param {String} fromFilename
+ * @param {String} toFilename
+ */
+var copyFile = (fromFilename, toFilename) => new Promise(function(resolve, reject) {
+	setTimeout(function() {
+		copyFilePromise(fromFilename, toFilename, resolve, reject);
+	}, 1000);
+})
+
 /*
 quick test code - 
 isFile("test/afile.txt").then(function(value) {
@@ -29,7 +52,15 @@ isFile("test/afile.txt").then(function(value) {
 	console.log(value);
 });
 */
-
+/*
+copyFile("test/afile.txt", "test/afile1.txt").then(function(value) {
+	console.log("done");
+	console.log(value);
+}).catch(function(err) {
+	console.log(err);
+});
+*/
 if (typeof module !== 'undefined' && module.exports != null) {
-    exports.isFile = isFile;
+	exports.isFile = isFile;
+	exports.copyFile = copyFile;
 }
--- test/test.js
+++ test/test.js
@@ -2,7 +2,11 @@
 var assert = require('assert');
 var fileOps = require('../index');
 var isFile = fileOps.isFile;
+var copyFile = fileOps.copyFile;
 
+/*****************\
+| Tests of isFile |
+\*****************/
 describe('test when input is a file', function() {
   describe('#isFile()', function() {
     it('should return true when input is a file', function() {
@@ -26,6 +30,39 @@ describe('test when input does not exist as a file', function() {
         // Check for the specific error message.
         assert.equal(err.message.indexOf('ENOENT: no such file or directory, stat'), 0,
           "Expected ENOENT error, but got '" + err.message + "'");
+      });
+    });
+  });
+});
+
+/*******************\
+| Tests of copyFile |
+\*******************/
+describe('test when input to-file does not exist', function() {
+  describe('#copyFile()', function() {
+    it('should return true when copy-to file does not exist', function() {
+      var toFilename = "./test/afile1.txt";
+      var fromFilename = "./test/afile.txt";
+      return copyFile(fromFilename, toFilename).then(function(value) {
+        assert.strictEqual(value, undefined);
+      }).catch(function(err) {
+        throw new Error('Unexpected error condition; copy-to file should not exist: ' + toFilename);
+      });
+    });
+  });
+});
+
+describe('test when input to-file does exist', function() {
+  describe('#copyFile()', function() {
+    it('should fail when copying over existing file', function() {
+      var toFilename = "./test/afile.txt";
+      var fromFilename = "./test/afile.txt";
+      return copyFile(fromFilename, toFilename).then(function(value) {
+        throw new Error('Unexpected error condition: should not be able to copy');
+      }).catch(function(err) {
+        // Check for the specific error message.
+        assert.equal(err.message.indexOf('EEXIST: file already exists'), 0,
+          "Expected EEXIST error, but got '" + err.message + "'");
       });
     });
   });
